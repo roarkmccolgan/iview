aaea990a25e4a8a3be5c85d6576904dfb9765ba9
roarkmccolgan <roarkmccolgan@gmail.com>
2016-11-22 11:27:47 +0200

    quick fix

--- resources/views/tool/default/report/report.blade.php
+++ resources/views/tool/default/report/report.blade.php
@@ -24,7 +24,29 @@
 			</tr>
 		</table>
 		@endif
-		
+		<div class="section group {{$section['seckey']}}" style="">
+			@if($section['designline'])
+			<div style="position: absolute; top:0mm; right: {{$section['image'] ? '-45mm':'-50mm' }}; width: 130mm; height: 3mm; background-color: {{$section['color']}}"></div>
+			@endif
+			@if($section['pb'])
+			<div class="spacer"></div>
+			@endif
+			@if($section['hidetitle'])
+			<h2 class="{{$section['seckey']}}">{{$section['title']}}</h2>
+			@endif
+			<h4>{{$section['rating']}}</h4>
+			@if($section['introduction'])
+			{!!$section['introduction']!!}
+			@endif
+			
+			@if($section['graph'])
+				<div id="{{$section['seckey']}}-div" class="graph" style="">
+					
+				</div>
+				@columnchart($section['seckey'].'_graph', $section['seckey'].'-div')
+			@endif
+			{!!$section['paragraph']!!}
+		</div>
 		@endforeach
 		<script src="{{ asset('js/vendor/jquery-1.10.1.min.js')}}"></script>
 		<script type="text/javascript">
