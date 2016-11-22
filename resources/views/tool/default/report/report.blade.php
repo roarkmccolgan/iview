7b9cb8d40dc6b23ba8472da8eacc4337d6c403bb
roarkmccolgan <roarkmccolgan@gmail.com>
2016-11-22 11:00:57 +0200

    quick fix

--- resources/views/tool/default/report/report.blade.php
+++ resources/views/tool/default/report/report.blade.php
@@ -6,10 +6,49 @@
     	<meta name="description" content="">
     	<meta name="author" content="">
 		<title>Report</title>
-		
 	</head>
 	<body style="margin: 0; padding: 0;">
-		<h1>Dude!</h1>
+		
+		<div class="spacer"></div>
+		<div class="introduction">
+			<h2>Introduction {{session('product.id')}}</h2>
+			{!!trans(session('product.alias').'.introduction')!!}
+		</div>
+		@foreach($sections as $key=>$section)
+		<div class="{{$section['pb'] && !$section['pageimage'] ?'pb ':''}}"></div>
+		@if($section['pageimage'])
+		<table class="{{$section['pb'] ?'pb ':''}}" style="width: 100%;" cellpadding="0" cellspacing="0">
+			<tr>
+				
+			</tr>
+		</table>
+		@endif
+		<div class="section group {{$section['seckey']}}" style="">
+			@if($section['designline'])
+			<div style="position: absolute; top:0mm; right: {{$section['image'] ? '-45mm':'-50mm' }}; width: 130mm; height: 3mm; background-color: {{$section['color']}}"></div>
+			@endif
+			@if($section['pb'])
+			<div class="spacer"></div>
+			@endif
+			@if($section['hidetitle'])
+			<h2 class="{{$section['seckey']}}">{{$section['title']}}</h2>
+			@endif
+			<h4>{{$section['rating']}}</h4>
+			@if($section['introduction'])
+			{!!$section['introduction']!!}
+			@endif
+			@if($section['image'])
+			
+			@endif
+			@if($section['graph'])
+				<div id="{{$section['seckey']}}-div" class="graph" style="">
+					
+				</div>
+				@columnchart($section['seckey'].'_graph', $section['seckey'].'-div')
+			@endif
+			{!!$section['paragraph']!!}
+		</div>
+		@endforeach
 		<script src="{{ asset('js/vendor/jquery-1.10.1.min.js')}}"></script>
 		<script type="text/javascript">
 			$(function() {
