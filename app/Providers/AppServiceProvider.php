a830c0a1f269d435b67a556d216d6e0aa2b139c6
roarkmccolgan <roarkmccolgan@gmail.com>
2019-04-10 07:51:46 +0200

    Italy Layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Collection::macro('implodeLast', function ($value, $glue = null, $lastGlue = null) {
+            /** @var Collection $collection */
+            $collection = $this;
+            $first      = $collection->first();
+
+            if (\is_array($first) || \is_object($first)) {
+                return $collection->pluck($value)->implodeLast($glue, $lastGlue);
+            }
+
+            if ($collection->count() >= 2) {
+                return $collection->slice(0, -1)->implode($value) . $glue . $collection->last();
+            }
+
+            return implode($value, $collection->all());
+        });
     }
 
     /**
