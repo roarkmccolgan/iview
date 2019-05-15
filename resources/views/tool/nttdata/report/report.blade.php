433625761e6e1804fbbf797a3e8111b4e03a4607
roarkmccolgan <roarkmccolgan@gmail.com>
2019-05-15 11:04:12 +0200

    Country Graph NTTDataDX

--- resources/views/tool/nttdata/report/report.blade.php
+++ resources/views/tool/nttdata/report/report.blade.php
@@ -56,9 +56,9 @@ $manifest = json_decode($json, TRUE);
 	            Your result compared with your peers
 	        </p>
             {!!$overallGraph!!}<br/>
-            @isset($countryGraph)
+            @if(isset($countryGraph))
 			    {!!$countryGraph!!}<br/>
-			@endisset
+			@endif
             {!!$industryGraph!!}<br/>
             {!!$employeeGraph!!}<br/>
             <div class="text-right mt-4">
