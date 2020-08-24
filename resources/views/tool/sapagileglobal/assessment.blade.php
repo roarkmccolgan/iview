8303ec183550030a794c8d2c66d5c3f502d41158
roarkmccolgan <roarkmccolgan@gmail.com>
2020-08-24 08:09:06 +0200

    SAP GLobal Tracking

--- resources/views/tool/sapagileglobal/assessment.blade.php
+++ resources/views/tool/sapagileglobal/assessment.blade.php
@@ -4,6 +4,13 @@
 
 
 @section('head')
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WF9SDK8');</script>
+<!-- End Google Tag Manager -->
 @parent
 <meta name="description" content="">
 <meta name="author" content="">
@@ -11,6 +18,10 @@
 @stop
 
 @section('main')
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF9SDK8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="app" class="h-screen flex flex-col relative">
 	<div class="bg-blackblack">
 	    <div class="container mx-auto px-4">
