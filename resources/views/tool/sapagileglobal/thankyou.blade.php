a3080524846b80e7d07cf42de0435d577f62c19a
roarkmccolgan <roarkmccolgan@gmail.com>
2020-08-28 08:18:54 +0200

    access

--- resources/views/tool/sapagileglobal/thankyou.blade.php
+++ resources/views/tool/sapagileglobal/thankyou.blade.php
@@ -4,6 +4,13 @@
 
 
 @section('head')
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WF9SDK8');</script>
+<!-- End Google Tag Manager -->
 @parent
 <meta name="description" content="">
 <meta name="author" content="">
@@ -25,6 +32,10 @@
 	ga('send', 'event', "lead", "generated", utm);
 
 </script>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF9SDK8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @endif
 <div id="app" class="h-screen flex flex-col">
 	<div class="bg-blackblack">
@@ -91,6 +102,20 @@ s.parentNode.insertBefore(b, s);})();
 </script>
 <noscript>
 <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1726817&fmt=gif" />
+</noscript>
+<script type="text/javascript">
+_linkedin_partner_id = "2528004";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+(function(){var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})();
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=2528004&fmt=gif" />
 </noscript>
 @parent
 @stop
