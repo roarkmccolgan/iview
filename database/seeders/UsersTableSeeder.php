e8b6f1a570a35df1c9d82599fbd697eb88c6dce0
Laravel Shift <shift@laravelshift.com>
2021-02-13 03:29:25 +0000

    Shift cleanup

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
